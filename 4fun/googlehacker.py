8d56d1a45e274f583b02a1cda3c4d744d821a37a
LittleBoyBB <felipe_souza@mail.com>
2016-11-30 23:41:19 -0300

    Adicionando o script haskiador de google

---
+++ 4fun/googlehacker.py
@@ -0,0 +1,67 @@
+import os
+import time
+
+print("\n __                            __                           \n|  \                          |  \                          \n| $$____    ______    _______ | $$   __   ______    ______  \n| $$    \  |      \  /       \| $$  /  \ /      \  /      \ \n| $$$$$$$\  \$$$$$$\|  $$$$$$$| $$_/  $$|  $$$$$$\|  $$$$$$\n| $$  | $$ /      $$| $$      | $$   $$ | $$    $$| $$   \$$\n| $$  | $$|  $$$$$$$| $$_____ | $$$$$$\ | $$$$$$$$| $$      \n| $$  | $$ \$$    $$ \$$     \| $$  \$$\ \$$     \| $$      \n \$$   \$$  \$$$$$$$  \$$$$$$$ \$$   \$$  \$$$$$$$ \$$      \nt1m3 [@digitalgangsta]\n\n")
+
+def main():                                                            
+	i = raw_input("[*] Voce deseja hackear o google? > ")
+	if i == 'sim':
+		hask()
+	elif i == 'nao':
+		hask()
+	else:
+		print("[*] Digite apenas sim ou nao!")
+		main()
+		
+def hask():
+	
+	print("[*] Importando modulos para a hackinagem...")
+	time.sleep(2)
+	print("") 
+	time.sleep(1)
+	print("checking host system type... x86_64-unknown-linux-gnu")	
+	time.sleep(1)
+	print("checking whether the C compiler works... yes")	
+	time.sleep(1)
+	print("checking for suffix of executables... ")	
+	time.sleep(1)
+	print("checking for a BSD-compatible install... /usr/bin/install -c")	
+	time.sleep(1)
+	print("checking for ANSI C header files... yes")
+	time.sleep(1)
+	print("checking for sys/stat.h... yes")
+	time.sleep(1)
+	print("checking for memory.h... yes")
+	time.sleep(1)
+	print("checking for strings.h... yes")
+	time.sleep(1)
+	print("checking getopt.h presence... yes")
+	time.sleep(1)
+	print("checking for ANSI C header files... (cached) yes")
+	time.sleep(1)
+	print("checking for memset... yes")
+	time.sleep(1)
+	print("checking for socket... yes")
+	time.sleep(1)
+	print("checking for strcasecmp... yes")
+	time.sleep(1)
+	google()
+	
+def google():
+	print("HACKEANDO O GOOGLE AGUARDE........")
+	time.sleep(3)
+	x = 2
+	while x > 1:
+	
+		print("HACKEANDO O GOOGLE AGUARDE........\n[*  ]")
+		time.sleep(1)
+		os.system("clear")
+		print("HACKEANDO O GOOGLE AGUARDE........\n[ * ]")
+		time.sleep(1)
+		os.system("clear")
+		print("HACKEANDO O GOOGLE AGUARDE........\n[  *]")
+		time.sleep(1)
+		os.system("clear")
+
+if __name__ == "__main__":
+	main()
